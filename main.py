031524489b9187c9b629ad50649d5c345a9a138e
KryzPetr1533 <petrassassin03@gmail.com>
2022-09-10 23:59:42 +0300

    Adding new data and text editor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 text = open("data.txt", 'r', encoding='utf-8')
-text_readed = text.read()
+text_readed = text.read().lower()
 text_arr = re.split(r'\W+', text_readed)
-text_arr.remove('')
-# text_arr.pop()
+text_arr = [i for i in text_arr if i != '']
 print(text_arr)
+# text_arr.pop()
