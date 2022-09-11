332a9124affe5bd1a16a05724c55906ab445d2e0
KryzPetr1533 <petrassassin03@gmail.com>
2022-09-11 16:34:25 +0300

    Creating database

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ text = open("data.txt", 'r', encoding='utf-8')
 text_readed = text.read().lower()
 text_arr = re.split(r'\W+', text_readed)
 text_arr = [i for i in text_arr if i != '']
-print(text_arr)
-# text_arr.pop()
+dict_data = {}
+length = len(text_arr)
+for i in range(length-2):
+    pair = text_arr[i] + " " + text_arr[i+1]
+    if dict_data.get(pair, 0) is not 0:
+        if dict_data[pair].get(text_arr[i+2], 0) is not 0:
+            dict_data[pair][text_arr[i+2]] = dict_data[pair][text_arr[i+2]] + 1
+            print(dict_data[pair])
+    else:
+        dict_data[pair] = {text_arr[i+2]: 1}
